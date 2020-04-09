70e8944db3e390b288de21ec6ac1d6dfce0b6183
Brandon McGhee <55092109+bmcghee@users.noreply.github.com>
2020-04-09 10:45:42 -0400

    Add files via upload
    Scripts to move the character and can shot in one direction

---
+++ Scripts/Pickup.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+float throwForce = 50;
+Vector3 objectPos;
+float distance;
+public GameObject cube;
+public GameObject ball;
+public Rigidbody rb;
+private bool hitr = false;
+private bool press = false;
+
+  void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+ // Update is called once per frame
+ void Update () {
+     if(Input.GetKeyDown(KeyCode.F) && press == false)
+    {
+         press = true;
+    }
+  
+    if(hitr == true && press ==true)
+    {
+        ball.transform.position= new Vector3(transform.position.x+1,transform.position.y,transform.position.z);
+    }
+    if (Input.GetKeyDown("space") && press == true && hitr == true)
+        { 
+            press = false;
+            hitr = false;
+            ball.GetComponent<Rigidbody>().AddForce(transform.forward * throwForce);
+        }
+ }
+
+
+     void OnTriggerEnter(Collider other)
+    {
+        //turn the bol value to true when you hit one of the cubes
+        if(other.gameObject.CompareTag("ball"))
+        {
+            hitr = true;
+        }
+        
+    }
+}
---
+++ Scripts/move_character.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move_character : MonoBehaviour
+{
+    public float speed = 1f;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal")*Time.deltaTime*speed;
+         float z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(x,0,z);
+
+    }
+
+}
