972683ed1a72433b46e2994e7826fa00d294eb2f
Joshua Nguyen <37251640+JoshVNguyen@users.noreply.github.com>
2020-04-19 17:58:49 -0400

    Weapon Spawner Script

---
+++ Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject weaponPrefab;
+    public GameObject [] spawners;
+    private int index;
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", 1.0f, 5.0f);
+    }
+
+    void Spawn()
+    {
+        index = Random.Range (0, spawners.Length);
+        Vector3 spawnLocation = spawners[index].transform.position;
+        spawnLocation.y += 10;
+
+        Instantiate(weaponPrefab, spawnLocation, Quaternion.identity);
+    }
+}
