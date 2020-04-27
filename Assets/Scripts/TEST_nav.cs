dc60d879e08e9824eaa6d4cd7f77816486afd386
emcanin <33500232+emcanin@users.noreply.github.com>
2020-04-27 13:38:07 -0400

    Testing Navigation
    The enemy now has a basic test script written to follow the player using the navmesh. To do so, all I did was add the navigation script and add a navmeshagent component to the Enemy prefab

---
+++ Assets/Scripts/TEST_nav.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TEST_nav : MonoBehaviour
+{
+
+    public GameObject target_player;
+
+    private NavMeshAgent nav_agent;
+    private Vector3 destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.nav_agent = this.GetComponent<NavMeshAgent>();
+        this.destination = this.target_player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.destination = this.target_player.transform.position;
+        this.nav_agent.destination = this.destination;
+    }
+}
